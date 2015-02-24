7764dffdecde87adfa9174cb07e2e8e4b6210efd
Rahul Anand <et@eternalthinker.co>
2015-02-24 12:58:52 +0530

    Added styling to the form

--- src/main/java/com/proto/ui/CampaignUploadHandler.java
+++ src/main/java/com/proto/ui/CampaignUploadHandler.java
@@ -13,43 +13,70 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ContextHandlerCollection;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
 
 public class CampaignUploadHandler extends AbstractHandler {
-    
+
     private static final MultipartConfigElement MULTI_PART_CONFIG = 
             new MultipartConfigElement(System.getProperty("java.io.tmpdir"));
-    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd.HH.mm");
 
     public void handle(String target, Request baseRequest, HttpServletRequest request,
             HttpServletResponse response) throws IOException, ServletException {
-
+        
+        // A simple method to serve static content like CSS, JS files
+        if (target.startsWith("/resource")) {
+            serveStatic(target, response, baseRequest);
+            return;
+        }
+        
         // Begin response content
         StringBuilder content = new StringBuilder();
-        content.append("<!DOCTYPE html>\n<html><body>");
+        content.append("<!DOCTYPE html>\n<html><body><head>");
+
+        // HEAD
+        try {
+            content.append(Files.toString(new File("static/head.html"), Charsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        content.append("<title>Video Campaign Upload</title></head>");
+
+        // CONTENT WRAPPER TOP
+        content.append("<div class='container-fluid' style='height:100%;'>");
+        content.append("<div class='row' style='height:100%;'>");
+        content.append("<div class='col-xs-12' style='height:100%;'>");
 
         // Process upload request
         if ( request.getContentType() != null && 
                 request.getContentType().startsWith( "multipart/form-data" )) {
+            // INFO START
+            content.append("<div style='margin: 20px auto'><span class='alert alert-success'>");
+
             baseRequest.setAttribute(Request.__MULTIPART_CONFIG_ELEMENT, MULTI_PART_CONFIG);
-            
+
             System.out.println("Request params found:");
             for (Part part : request.getParts()) {
                 System.out.println("  * " + part.getName());
             }
-            
+
             String campId = request.getParameter("camp-id");
-            content.append("Details of submitted campaign: " + campId + "<br>");
-            
+            content.append("Details of submitted campaign: " + campId + ": ");
+
             Date now = new Date();
             String dirName = dateFormat.format(now);
-            
+
             File destDir = new File(dirName);
             if (! destDir.exists()) {
                 try{
@@ -65,27 +92,36 @@ public class CampaignUploadHandler extends AbstractHandler {
                 InputStream fileStream = pidFilePart.getInputStream(); 
                 File dest = new File(dirName + "/" + campId + "_pid.txt");
                 ByteStreams.copy(fileStream, Files.newOutputStreamSupplier(dest));
-                content.append("Saved pid file..<br>");
+                content.append("Saved pid file. ");
             }
-            
+
             Part videoFilePart = request.getPart("video-file");
             if (videoFilePart.getSize() > 0) {
                 System.out.println("Saving video file..");
                 InputStream fileStream = videoFilePart.getInputStream(); 
                 File dest = new File(dirName + "/" + campId + "_video.flv");
                 ByteStreams.copy(fileStream, Files.newOutputStreamSupplier(dest));
-                content.append("Saved video file..<br>");
+                content.append("Saved video file.");
             }
-            
-            content.append("<hr />");
+
+            // INFO END
+            content.append("</span></div>");
+            //content.append("<hr />");
         }
         // End of upload request processing
 
+
         try {
-            content.append(Files.toString(new File("static/upload_form.html"), Charsets.UTF_8));
+            content.append(Files.toString(new File("static/upload_form2.html"), Charsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // CONTENT WRAPPER END
+        content.append("</div>");
+        content.append("</div> <!-- END core content row -->");
+        content.append("</div> <!-- END main container -->");
+
         content.append("</body></html>");
         // End of response content
 
@@ -96,11 +132,22 @@ public class CampaignUploadHandler extends AbstractHandler {
         baseRequest.setHandled(true);
     }
 
+    private void serveStatic(String path, HttpServletResponse response, Request baseRequest) {
+        try {
+            response.getWriter().println(Files.toString(new File(path.substring(1)), Charsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        response.setContentType("text/plain;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+    }
 
     public static void main(String[] args) {
         Server server = new Server(8080);
+
         server.setHandler(new CampaignUploadHandler());
-        
+
         try {
             server.start();
         } catch (Exception e) {
